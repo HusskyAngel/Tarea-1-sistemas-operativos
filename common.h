a4a51af95798debca21aade187764be452b8ddde
HusskyAngel <76019110+HusskyAngel@users.noreply.github.com>
2021-03-08 15:11:19 -0500

    Add files via upload

---
+++ common.h
@@ -0,0 +1,12 @@
+#ifndef __common_h__
+#define __common_h__
+
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <assert.h>
+
+double GetTime();
+
+void Spin(int howlong);
+
+#endif // __common_h__
